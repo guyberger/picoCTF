e110e23a80db57e6bb087f96dd9f1fe733a32f36
Guy Berger <guyber1992@gmail.com>
2019-04-08 12:06:48 -0700

    added makeFlag usage msg

--- makeFlag.py
+++ makeFlag.py
@@ -2,7 +2,10 @@
 
 import sys
 fout = open("flag.txt", "w+")
-if (len(sys.argv) > 1):
+if (len(sys.argv) = 2):
     flag = sys.argv[1]
     out = "picoCTF{" + flag + "}"
     fout.write(out)
+
+else:
+    print ("Format: python makeFlag.py FLAG")
